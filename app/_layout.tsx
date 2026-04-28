19c595795054db8084707f264fdd4c75ef822f9d
Mitchell Collings <macollings@gmail.com>
2026-04-28 23:59:05 +1000

    Change order of tab layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { Tabs } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
-export default function TabLayout() {
+export default function RootLayout() {
   return (
     <>
       <Tabs
@@ -12,20 +12,20 @@ export default function TabLayout() {
         }}
       >
         <Tabs.Screen
-          name="space"
-          options={{
-            title: "Motion",
-            tabBarIcon: ({ color }) => (
-              <FontAwesome name="arrows" size={24} color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen
           name="other"
           options={{
             title: "Other",
             tabBarIcon: ({ color }) => (
               <MaterialIcons name="sensors" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="space"
+          options={{
+            title: "Motion",
+            tabBarIcon: ({ color }) => (
+              <FontAwesome name="arrows" size={24} color={color} />
             ),
           }}
         />
